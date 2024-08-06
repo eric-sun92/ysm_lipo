0e49d2f8eacd87ed9e58f3109d38a2adb31bd834
Eric Sun <eric.sun.ecs92@yale.edu>
2024-08-06 17:53:17 -0400

    dist added

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -11,5 +10,5 @@ export default defineConfig({
       'venn.js': path.resolve(__dirname, './node_modules/venn.js/build/venn.js')
     }
   },
-  base: '/assets/',
+  base: '/',
 })
